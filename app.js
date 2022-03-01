b0f550e517bdda3c40064751e3582471793298fa
bibhutirajbhandari <bibhutirajbhandari@gamil.com>
2022-03-01 19:36:13 +0545

    my project

---
+++ app.js
@@ -0,0 +1,76 @@
+
+var titles = [
+    "Space Shooter Combat Game Python",
+    "Online PDF to Text Converter & Language Translator Python",
+    "Herb & Grocery Shopping Android App",
+    "Dementia Virtual Memory App",
+    "Android Messenger App",
+    "Barbershop Service Booking App",
+    "Fitness App With Workout Diet & Motivation",
+    "Search Engine",
+    "e-Authentication system",
+    "Cursor movement on object motion",
+    "Crime rate prediction",
+    "Android battery saver system" ]
+
+    function getTitle() {
+        var randomNumber = Math.floor(Math.random() * titles.length);
+        document.getElementById('newtitlesection').innerHTML = titles[randomNumber];
+    }
+
+    var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
